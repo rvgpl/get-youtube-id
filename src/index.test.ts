d4790577b2c413f266d3e959449fb5156994c15d
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-04-07 21:21:08 +0530

    Adds tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,34 @@
-import add from "./index";
+import getYouTubeID from "./index";
 
-describe("Add test", () => {
-  it("should add two numbers", () => {
-    const value = add(1, 2);
-    expect(value).toBe(3);
+const testCases = [
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/watch?v=LdGZFfzHtCM&feature=feedrec_grec_index" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/v/LdGZFfzHtCM?fs=1&amp;hl=en_US&amp;rel=0" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/watch?v=LdGZFfzHtCM#t=0m10s" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/embed/LdGZFfzHtCM?rel=0" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/watch?v=LdGZFfzHtCM" },
+  { id: "LdGZFfzHtCM", url: "http://youtu.be/LdGZFfzHtCM" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/v/LdGZFfzHtCM?fs=1&amp;hl=en_US&amp;rel=0" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/embed/LdGZFfzHtCM?rel=0" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/watch?v=LdGZFfzHtCM&feature=feedrec_grec_index" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/watch?v=LdGZFfzHtCM" },
+  { id: "LdGZFfzHtCM", url: "http://youtu.be/LdGZFfzHtCM" },
+  { id: "LdGZFfzHtCM", url: "http://www.youtube.com/watch?v=LdGZFfzHtCM#t=0m10s" }
+];
+
+describe("getYouTubeId", () => {
+  it("should return null if url is not from youtube", () => {
+    const youtubeID = getYouTubeID("https://www.google.com");
+    expect(youtubeID).toBe(null);
+  });
+  it("should return null if a malformed youtube url", () => {
+    const youtubeID = getYouTubeID("https://www.youtube.com/ss");
+    expect(youtubeID).toBe(null);
+  });
+
+  it("should return id for correct url", () => {
+    testCases.forEach((test) => {
+      const youtubeID = getYouTubeID(test.url);
+      expect(youtubeID).toBe(test.id);
+    });
   });
 });
