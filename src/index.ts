0e57377df37a5f52a15d432f673ba4cffb466296
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-04-07 21:21:32 +0530

    Adds get youtube id

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-const add = (a: number, b: number): number => a + b;
+const URLPatterns = [
+  /youtu\.be\/([^#\&\?]{11})/, // youtu.be/<id>
+  /\?v=([^#\&\?]{11})/, // ?v=<id>
+  /\&v=([^#\&\?]{11})/, // &v=<id>
+  /embed\/([^#\&\?]{11})/, // embed/<id>
+  /\/v\/([^#\&\?]{11})/ // /v/<id>
+];
 
-export default add;
+const getYouTubeId = (url: string): null | string => {
+  let youTubeID = null;
+  //Make sure the url is from youtube
+  if (/youtu\.?be/.test(url)) {
+    URLPatterns.forEach((pattern) => {
+      if (pattern.test(url)) {
+        const exec = pattern.exec(url);
+        youTubeID = exec ? exec[1] : null;
+      }
+    });
+  }
+  return youTubeID;
+};
+
+export default getYouTubeId;
